41582431e9811e9e44b9c029d6e738771e1082c0
aniruddhadeshmukh2201 <aniruddhadeshmukh2201@gmail.com>
2025-05-15 19:56:18 +0530

    updated gitignore and tried out lexer

--- flint-lang/src/main/java/dev/flint/App.java
+++ flint-lang/src/main/java/dev/flint/App.java
@@ -1,11 +1,23 @@
 package dev.flint;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.flint.lexer.Lexer;
+import dev.flint.lexer.Token;
 
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String program = "2+3;";
+        Lexer l = new Lexer(program);
+        List<Token> t = new ArrayList<>();
+        t = l.tokenize();
+        for(int i = 0; i < t.size(); i++) {
+            System.out.println(t.get(i));
+        }
     }
 }
 
